148a8040ad18e2108eb6b8dee92b13db9f9690f4
Tom.Li <open222333@gmail.com>
2024-01-15 00:30:07 +0800

    新增 預設處理資料夾路徑

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ config = ConfigParser()
 config.read(os.path.join('conf', 'git_config.ini'), encoding='utf-8')
 USERNAME = config.get('GIT', 'USERNAME')
 PASSWORD = config.get('GIT', 'PASSWORD')
-DIR_PATH = config.get('GIT', 'DIR_PATH')
+DIR_PATH = config.get('GIT', 'DIR_PATH', fallback='repo')
 GITDOMAIN = config.get('GIT', 'GITDOMAIN', fallback='github.com')
 # 設定紀錄log等級 DEBUG,INFO,WARNING,ERROR,CRITICAL 預設WARNING
 LOG_LEVEL = config.get('GIT', 'LOG_LEVEL', fallback='WARNING')
